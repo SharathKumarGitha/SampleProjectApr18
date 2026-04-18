efeff0da3ab63341c82574368e7cca3c3926da80
sharathgitha627 <sharathgitha1627@gmail.com>
2026-04-18 12:08:27 +0530

    first commit

---
+++ src/main/java/com/example/SampleProjectApplication.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SampleProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleProjectApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String home() {
+        return "Hello Jenkins";
+    }
+}
